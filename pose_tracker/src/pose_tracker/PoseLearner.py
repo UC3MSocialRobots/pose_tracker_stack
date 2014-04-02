fa37af31be45c54749de31b8f8698e8c72ef15f6
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-04-02 11:18:21 +0000

    added func to stub

--- pose_tracker/src/pose_tracker/PoseLearner.py
+++ pose_tracker/src/pose_tracker/PoseLearner.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
-import pandas as pd
-import 
 
 DEFAULT_NAME = 'pose_learner'
+
+
+def create_model(df, predictor):
+    '''
+        @type df: pandas.DataFrame
+        @param df the dataframe from which the model will be created
+        @param predictor: the algorithm that will be used to learn
+    '''
+    pass
+
 
 class PoseLearner():
     ''' Class produces a learned model from a entered DataFrame
@@ -12,3 +20,4 @@ class PoseLearner():
     '''
     def __init__(self, **kwargs):
         name = kwargs.get('node_name', DEFAULT_NAME)
+
