ceb55b72595bc15c3ec4e07122b721205e341ddd
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-04-02 11:18:28 +0000

    PEP8 Compliance

--- pose_tracker/src/pose_tracker/PoseDatasetIO.py
+++ pose_tracker/src/pose_tracker/PoseDatasetIO.py
@@ -5,15 +5,16 @@
 # import numpy as np
 import pandas as pd
 import datetime
- 
+
+
 def parse_date(date):
     ''' An utility function to parse the date used in the dataset metadata.
-        
-        @param date: The date in a form of a string. Allowed formats follow ISO8601
+
+        @param date: The date in a form of a string. Must follow ISO8601:
             1. I{YYYY-MM-DD}
-            2. I{YYYY-MM-DD hh:mm} 
+            2. I{YYYY-MM-DD hh:mm}
             3. I{YYYY-MM-DD hh:mm:ss}
-        @see: ISO 8601 for more information. 
+        @see: ISO 8601 for more information.
         @return: the entered date in form of "YYYY-MM-DD hh:mm:ss"
         @raise ValueError: if entered date is malformed
         '''
@@ -23,14 +24,14 @@ def parse_date(date):
             parsed_date = datetime.datetime.strptime(str(date), fmt)
             return parsed_date.strftime(fmt)
         except ValueError:
-            pass # Keep trying formats 
+            pass  # Keep trying formats
 
     # If we arrive here it means that an unsupported format was entered
-    raise ValueError("Incorrect date: {}. " 
-                        "Allowed formats: "
-                        "'YYYY-MM-DD', 'YYYY-MM-DD hh:mm' "
-                        "and 'YYYY-MM-DD hh:mm:ss'."
-                        " See ISO 8601 For more information.".format(date))
+    raise ValueError("Incorrect date: {}. "
+                     "Allowed formats: "
+                     "'YYYY-MM-DD', 'YYYY-MM-DD hh:mm' "
+                     "and 'YYYY-MM-DD hh:mm:ss'."
+                     " See ISO 8601 For more information.".format(date))
 
 
 class PoseDatasetIO(object):
@@ -38,9 +39,9 @@ class PoseDatasetIO(object):
     def __init__(self, *args, **kwargs):
         ''' Inits the class.
 
-            @param dataset: the name of the file where the dataset will be written
-            @param dataset_columns: The name of the columns for the dataset_table
-            
+            @param dataset: name of the file where the dataset will be written
+            @param dataset_columns: name of the columns for the dataset_table
+
             @raise KeyError: if some argument is missing
             @raise TypeError: if dataset is not string or dataset_columns is
             not an iterable.'''
@@ -49,10 +50,10 @@ class PoseDatasetIO(object):
         self.dataset_columns = kwargs['columns']
         # print self.dataset
         # print str(self.dataset_columns)
-        
+
         if not isinstance(self.dataset, str):
             raise TypeError("dataset must be a string")
-        if not hasattr(self.dataset_columns,'__iter__'):
+        if not hasattr(self.dataset_columns, '__iter__'):
             raise TypeError("dataset columns must be iterable!")
 
         # self.create_dataset(self.dataset)
@@ -62,7 +63,7 @@ class PoseDatasetIO(object):
         # print "CWD: " + os.getcwd()
         # print self.dataset
         self.store = pd.HDFStore(self.dataset)
-      
+
     def close(self):
         '''Closes the dataset file'''
         self.store.close()
@@ -70,23 +71,22 @@ class PoseDatasetIO(object):
     # def prepare_dataset(self, **kwargs):
     def fill_metadata(self, **kwargs):
         ''' Fills the metadata of the dataset.
-            
+
             @keyword creator: The creator of the dataset
             @keyword date: Date in "YYYY-mm-ddTHH:MM" (ISO 8601 date format)'
             @keyword descr: string based description of the dataset content
        '''
         creator = kwargs.get('creator', 'Anonymous')
         date = kwargs.get('date', '1900-01-01 00:00')
-        user_descr = kwargs.get('descr', 
-                                 "Default description. " \
-                                 "User forgot to add it when created " \
-                                 "the dataset.")
-        
-        # A dirty hack to use the pandas interface to pytables
-        description = pd.Series((creator, date, user_descr), 
-                                index=('creator', 'date','descr'))
-        self.store.put('description', description) 
+        user_descr = kwargs.get('descr',
+                                "Default description. "
+                                "User forgot to add it when created "
+                                "the dataset.")
 
+        # A dirty hack to use the pandas interface to pytables
+        description = pd.Series((creator, date, user_descr),
+                                index=('creator', 'date', 'descr'))
+        self.store.put('description', description)
 
     def get_metadata(self):
         ''' Returns the metadata table from the file
@@ -95,10 +95,10 @@ class PoseDatasetIO(object):
             @return: the metadata contained in the dataset file
             @rtype: pandas:Series'''
         return self.read_table('description')
-    
+
     def write(self, table_name, chunk, **kwargs):
         ''' Writes a chunk of data to the dataset file
-            
+
             @param table_name: the name of the table on the file
             @ptype chunk: pandas.DataFrame
             @param chunk: the pandas.DataFrame to be written to the file
@@ -107,7 +107,7 @@ class PoseDatasetIO(object):
                 @see 'pandas.io.pytables.HDFStore.put().
             @raise TypeError: if chunk is not a pandas.DataFrame
             @raise ValueError: if chunk is empty
-            
+
             '''
         if not isinstance(chunk, pd.DataFrame):
             raise TypeError("chunk is not a pandas.DataFrame. Could not write")
@@ -117,11 +117,10 @@ class PoseDatasetIO(object):
             raise ValueError("data chunk is empty. Nothing to write")
 
         self.store.put(table_name, chunk, **kwargs)
-        
-        
+
     def read_table(self, table_name):
         ''' Reads the file and returns a dataframe stored in table table_name
-        
+
         @type table_name: string
         @param table_name: The table where to read
         @return: a pandas dataframe obtained from table table_name
