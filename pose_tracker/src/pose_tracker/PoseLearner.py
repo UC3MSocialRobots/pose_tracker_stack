e870aabe7ae09f7b5bc1ff6092b68b59d14e1ced
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-04-17 12:07:16 +0000

    PoseLearner produces a fitted model

--- pose_tracker/src/pose_tracker/PoseLearner.py
+++ pose_tracker/src/pose_tracker/PoseLearner.py
@@ -1,23 +1,75 @@
 #!/usr/bin/env python
 
+from __future__ import (print_function, division)
+
+from functools import partial
+import numpy as np
+import pandas as pd
+
+# import PoseDatasetIO as pdio
+from PoseDatasetIO import PoseDatasetIO
+import user_data_loader as udl
+
+
 DEFAULT_NAME = 'pose_learner'
+COLS_TO_CLEAN=udl.confidences   
+
+def _clean_prefix(text, prefix): 
+    return text.lstrip(prefix)
+
+_rm_stand_pref = partial(_clean_prefix, prefix='STAND_')
 
 
-def create_model(df, predictor):
+def prepare_dataset(filename, group_name):
+    ''' Loads the file filename and returns all the tables contained in the 
+        group 'group_name' in form of a unified dataset
+        prior to returning it, the dataset is grouped by pose, to 
     '''
-        @type df: pandas.DataFrame
-        @param df the dataframe from which the model will be created
-        @param predictor: the algorithm that will be used to learn
+    with PoseDatasetIO(dataset=filename, columns=udl.index, mode='r') as dataset:
+        dataset = {node._v_name: dataset.store.select(node._v_pathname). \
+                                              groupby('pose').mean(). \
+                                              rename(_rm_stand_pref)
+                  for node in dataset.store.get_node(group_name) }
+        return pd.concat(dataset)
+
+
+def drop_columns(dataset, cols=COLS_TO_CLEAN):
+    ''' Drops the entered dataset columns. 
+        @type dataset: pandas.DataFrame
+        @param dataset: the dataset which columns are going to be dropped 
+        @param cols: the list of column names to be dropped.
+                     Default: L{COLS_TO_CLEAN}
+        @return: A copy of the dataset without the columns
     '''
-    pass
+    return dataset.drop(cols, axis=1)
 
 
-class PoseLearner():
-    ''' Class produces a learned model from a entered DataFrame
+def numerize_y(y):
+    ''' Converts vector y to nums'''
+    labels = sorted(set(y))
+    return np.array(map(labels.index, y))
 
-        @type df: pandas.DataFrame
-        @keyword df: 
+
+def train_clf(dataset, **kwargs):
     '''
-    def __init__(self, **kwargs):
-        name = kwargs.get('node_name', DEFAULT_NAME)
+        Trains a classifier with the entered data
+        @param dataset: 
+        @type dataset:
+        @keyword model:
+        @type model:
+        @keyword param_grid: Dictionary of parameters to 
+        @type param_grid: dict
 
+        @return: the classifier already fitted to the entered data
+    '''
+    model = kwargs.get('predictor', 
+                        ensemble.RandomForestClassifier(oob_score=True))
+    param_grid = kwargs['param_grid']
+
+    X = dataset.values
+    y = zip(*dataset.index)[1]
+    y_num = numerize_y(y)
+    
+    classif = GridSearchCV(model, param_grid, cv=3, score_func=sklm.f1_score)
+    classif.fit(X, y_num)
+    return classif
