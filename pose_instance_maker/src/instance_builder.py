40e6ffb03e73c537e7150212415ed4785e571aea
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-05-07 10:39:45 +0000

    [FEAT]: InstanceBuilder for pi_tracker node

--- pose_instance_maker/src/instance_builder.py
+++ pose_instance_maker/src/instance_builder.py
@@ -4,18 +4,43 @@ import roslib; roslib.load_manifest('pose_instance_maker')
 #import rospy
 #from rospy import (logdebug, loginfo, logwarn, logerr, logfatal)
 
-from itertools import (chain, izip, starmap)
-from more_itertools import flatten
+from itertools import (izip, starmap)
+from toolz import (concat, cons)
 
 from pi_tracker.msg import Skeleton
 from pose_instance_maker.msg import PoseInstance
 
 def parse_label(label):
     ''' Parses the entered label.
-        @return: str(label) or "UNKNOWN" if the label is empty.'''
+        @return: str(label) or "UNKNOWN" if the label is empty.
+        @rtype: str'''
     if not label:
         return 'UNKNOWN'
     return str(label)
+
+
+class IBuilder():
+    ''' Base class that defines the interface to that other builders
+        should implement.
+    '''
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def parse_msg(self, msg, label):
+        ''' Gets msg and returns an instance message
+            @param msg: The message to be converted to a PoseInstance
+                        Each Builder will be able to process a single msg type
+                        Thus, the type of msg will depend on the Builder.
+            @return: The instance message filled with the msg data
+            @rtype: pose_instance_maker.msg.PoseInstance
+        '''
+        pass
+
+    def get_msg_class():
+        ''' Should return a 
+        '''
+        pass
+
 
 class PiTrackerIBuilder():
     def __init__(self, *args, **kwargs):
@@ -24,6 +49,10 @@ class PiTrackerIBuilder():
     def parse_msg(self, msg, label):
         ''' Parses a pi_tracker.msg.Skeleton message and converts it to a 
             L{PoseInstance} message
+            @name msg: The message to be parsed
+            @type msg: pi_tracker.msg.Skeleton
+            @return: The instance message already formatted
+            @rtype: pose_instance_maker.msg.PoseInstance
         '''
         if not isinstance(msg, Skeleton):
             raise TypeError("Messge is not a pi_tracker/msg/Skeleton.msg")
@@ -31,10 +60,13 @@ class PiTrackerIBuilder():
             raise TypeError("Label is not a string")
 
         msg_fields = izip(msg.position, msg.orientation, msg.confidence)
-        instance = chain(msg.user_id, flatten(starmap(self._parse, msg_fields)))
+        instance = cons(msg.user_id, concat(starmap(self._parse, msg_fields)))
         return PoseInstance(columns=msg.name, 
                             label=parse_label(label),
                             instance=list(instance))
+
+    def get_msg_class(self):
+        return Skeleton
 
     def _parse(self, position, orientation, confidence):
         return (position.x, position.y, position.z,
--- pose_instance_maker/src/test/test_instance_builder.py
+++ pose_instance_maker/src/test/test_instance_builder.py
@@ -3,7 +3,10 @@ import roslib; roslib.load_manifest(PKG)
 
 
 import unittest
-from numpy.random import rand as arand
+from numpy import linspace
+from itertools import repeat
+from more_itertools import ncycles
+from toolz import (concat, cons)
 
 from instance_builder import (parse_label, PiTrackerIBuilder, KinectIBuilder)
 from pi_tracker.msg import Skeleton
@@ -17,11 +20,14 @@ class TestParseLabel(unittest.TestCase):
         super(TestParseLabel, self).__init__(*args)
     
     def test_parse_label_returns_UNKNOWN_label(self):
+        self.assertEqual('UNKNOWN', parse_label('UNKNOWN'))
+        self.assertEqual('UNKNOWN', parse_label(None))
+        self.assertEqual('UNKNOWN', parse_label([]))
+
+    def test_parse_label(self):
         self.assertEqual('lalala', parse_label('lalala'))
         self.assertEqual('123', parse_label(123))
         self.assertEqual('123.0', parse_label(123.0))
-        self.assertEqual('UNKNOWN', parse_label(None))
-        self.assertEqual('UNKNOWN', parse_label([]))
 
 
 class TestPiTrackerIBuiler(unittest.TestCase):
@@ -39,54 +45,50 @@ class TestPiTrackerIBuiler(unittest.TestCase):
         
     def _make_skeleton_msg(self):
         ''' Helper method to produce facke Skeleton msgs '''
-        skel = Skeleton(user_id=0,
-                        name=['c0', 'c1', 'c2', 'c3', 'c4'],
-                        confidence=map(float, xrange(5)))
-        for _ in xrange(5):
-            skel.position.append(Vector3(*map(float, xrange(3))))
-            skel.orientation.append(Quaternion(*map(float, xrange(4))))
-        return skel
+        return Skeleton(user_id=0, name=['c0', 'c1', 'c2', 'c3', 'c4'],
+                        confidence=linspace(1,1,5),
+                        position=list(repeat(Vector3(*linspace(1,3,3)), 5)),
+                        orientation=list(repeat(Quaternion(*linspace(1,4,4)),5)))
 
     def _make_instance(self):
-        instance = PoseInstance(label='lalala',
-                                columns=['c0', 'c1', 'c2', 'c3', 'c4'])
-        for i in xrange(5):
-            for j in xrange(3):             # position: Vector3
-                instance.columns.append(j)   
-            for j in xrange(4):             # orientation: Quaternion
-                instance.columns.append(j)   
-            instance.columns.append(i)      # confidence
-        return instance
-
-    def test_parse_msg_peconditions(self):
-        with self.assertRaises(TypeError):
-            self.builder.parse_msg(None, 'lalala')
-        with self.assertRaises(TypeError):
-            self.builder.parse_msg(self.skel, None)
-        with self.assertRaises(TypeError):
-            self.builder.parse_msg(123, 'lalala')
-        with self.assertRaises(TypeError):
-            self.builder.parse_msg(self.skel, 1234)
-
+        ''' Fill instance with:
+                user_id and 5 fake positions, orientaitons and confidences
+            Position is a Vector3(x, y, z)
+            Orientation is a Quaternion(x, y, z, w)
+            Confidence is one field'''
+        field_lens = (3,4,1)   # Lengths of Position, Orientation and Confidence
+        inst = ncycles(concat(linspace(1,l,l) for l in field_lens), 5)
+        inst = cons(0, inst)   # Insert user_id at the beginning of instance
+        return PoseInstance(label    = 'lalala',
+                            columns  = ['c0', 'c1', 'c2', 'c3', 'c4'],
+                            instance = list(inst))
+        
+    def test_parse_msg_raises_TypeError_with_bad_peconditions(self):
+        bad_msgs = ((None, 'lalala'), (self.skel, None),
+                    (123, 'lalala'), (self.skel, 1234))
+        for instance, label in bad_msgs:
+            with self.assertRaises(TypeError):
+                self.builder.parse_msg(instance, label)
+        
     def test_parse_msg(self):
         self.assertEqual(self.instance, 
                          self.builder.parse_msg(self.skel, 'lalala'))
 
 
-class TestKinectIBuilder(unittest.TestCase):
-    """Tests"""
-    def __init__(self, *args):
-        super(TestKinectIBuilder, self).__init__(*args)
+# class TestKinectIBuilder(unittest.TestCase):
+#     """Tests"""
+#     def __init__(self, *args):
+#         super(TestKinectIBuilder, self).__init__(*args)
 
-    def setUp(self):
-        pass
+#     def setUp(self):
+#         pass
         
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
 
 if __name__ == '__main__':
     import rosunit
     rosunit.unitrun(PKG, 'test_TestParseLabel', TestParseLabel)
     rosunit.unitrun(PKG, 'test_TestPiTrackerIBuiler', TestPiTrackerIBuiler)
-    rosunit.unitrun(PKG, 'test_KinectIBuilder', TestKinectIBuilder)
+    # rosunit.unitrun(PKG, 'test_KinectIBuilder', TestKinectIBuilder)
