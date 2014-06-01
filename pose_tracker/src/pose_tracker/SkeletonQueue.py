98b2f5a6cdb2e90c5b4e15b4db458d5a9d0b954c
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-06-01 19:07:20 +0000

    PEP8

--- pose_tracker/src/pose_tracker/SkeletonQueue.py
+++ pose_tracker/src/pose_tracker/SkeletonQueue.py
@@ -7,12 +7,13 @@ from itertools import chain
 
 import kinect.nite_skeleton_msg_utils as nsku
 
+
 class SkeletonQueue(object):
-    """Class that contains a queue of skeletons 
+    """Class that contains a queue of skeletons
        along with operations return its eleements as a pandas.DataFrame"""
     def __init__(self, joint_names, *args):
         super(SkeletonQueue, self).__init__()
-        self.skeleton_queue = col.deque([]) # to store (skeletons, label)
+        self.skeleton_queue = col.deque([])  # stores (skeletons, label)
         self.joint_names = joint_names
 
     def __len__(self):
@@ -29,14 +30,14 @@ class SkeletonQueue(object):
     def _check_joints(self, joint_names1, joint_names2):
         ''' Raises TypeError if entered parameters differ'''
         if set(joint_names1) != set(joint_names2):
-            rospy.logwarn("Message joint name does not match to the expected one")
+            rospy.logwarn("Message joint name != expected one")
             raise TypeError(
                 "Message does not have the expected joint names\n"
                 "Received: {}\nExpected: {}"
                 .format(str(joint_names1), str(joint_names2)))
 
     def _process_skeleton_msg(self, skeleton):
-        ''' Unpacks a skeleton message frome the queue, 
+        ''' Unpacks a skeleton message frome the queue,
             checks if joints are valid and returns its data '''
         try:
             data, joint_names = nsku.unpack_skeleton_msg(skeleton)
@@ -49,23 +50,23 @@ class SkeletonQueue(object):
         ''' Calculates how many elements to retrieve from squeleton queue'''
         if chunksize < 0 or chunksize > len(self.skeleton_queue):
             return len(self.skeleton_queue)
-        return chunksize 
+        return chunksize
 
     def _pop_from_queue(self, elements):
         ''' Generator that pops n elements from skeleton_queue.
             @note: this is a generator, meaning that it executes LAZYLY!
-            @note: It only grabs the first skeleton from the NiteSkeletonList msg
+            @note: only grabs the first skeleton from the NiteSkeletonList msg
         '''
         for _ in xrange(self._calc_chunksize(elements)):
             try:
                 skels, label = self.skeleton_queue.popleft()
                 skel_data = self._process_skeleton_msg(skels.skeletons[0])
-                yield chain(skel_data, label)   
+                yield chain(skel_data, label)
             except TypeError, e:
                 rospy.logwarn("Message not added to the dataset\n"
                               "Reason: {}".format(e))
             except IndexError, e:
-                rospy.logdebug("Skeleton Queue got emptied. Stoping")               
+                rospy.logdebug("Skeleton Queue got emptied. Stoping")
                 break
 
     def _prepare_chunk(self, chunksize=50, **kwargs):
@@ -75,27 +76,27 @@ class SkeletonQueue(object):
                 then yelds all elements of the queue
         '''
         if not self.skeleton_queue:
-            rospy.logdebug("The skeleton skeleton_queue is empty. Nothing to do.")
+            rospy.logdebug("Skeleton_queue is empty. Nothing to do.")
             return
 
         for skel_data in self._pop_from_queue(chunksize):
             yield skel_data
-        
+
     def _chunk_to_data_frame(self, chunk, columns):
         ''' Returns a pandas.DataFrame of chunksize elements.
             @note: This method expects its input from L{_prepare_chunk}
             @param chunk: a chunk of elements obtained from L{_prepare_chunk}
-            @param columns: list containing the names of the 
+            @param columns: list containing the names of the
                 pandas.DataFrame columns
             @return: the elements of queue in form of a pandas.DataFrame
             @rtype: pandas.Dataframe'''
         return pd.DataFrame(list(chunk), columns=columns)
 
     def pop_n_to_DataFrame(self, n, columns):
-        ''' Pops n elements from the queue 
+        ''' Pops n elements from the queue
             and returns them as a L{pandas.DataFrame}
             @param n: number of elements to retrieve from queue
-            @param columns: list containing the names of the pandas.DataFrame columns
+            @param columns: list with the names of the pandas.DataFrame columns
             @return: n elements from the queue in form of a pandas.DataFrame
             @rtype: pandas.DataFrame'''
         chunk = self._prepare_chunk(n)
--- pose_tracker/src/test/test_SkeletonQueue.py
+++ pose_tracker/src/test/test_SkeletonQueue.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 PKG = 'pose_tracker'
-import roslib; roslib.load_manifest(PKG)
+import roslib
+roslib.load_manifest(PKG)
 
 import unittest
 from mock import patch
@@ -17,13 +18,13 @@ class TestSkeletonQueue(unittest.TestCase):
         super(TestSkeletonQueue, self).__init__(*args)
         self.joint_names = ['head', 'neck']
         self.skq = skq.SkeletonQueue(self.joint_names)
-        
+
     def setUp(self):
         pass
-        
+
     def tearDown(self):
         self.skq.skeleton_queue.clear()
-        
+
     # @unittest.skip("Skpping this Test")
     def test_process_skeleton_msg(self):
         good_skel = nsku.generate_fake_skelmsg(self.joint_names)
@@ -37,17 +38,16 @@ class TestSkeletonQueue(unittest.TestCase):
 
     def test_process_skeleton_msg_raises_TypeError_if_no_skeleton_passed(self):
         self.assertRaises(TypeError, self.skq._process_skeleton_msg, None)
-    
-    def test_process_skeleton_msg_raises_TypeError_when_receives_an_empty_skeleton(self):
+
+    def test_process_skeleton_msg_raises_TypeError_if_receives_empty_skel(self):
         skel = kin.NiteSkeleton()
         self.assertRaises(TypeError, self.skq._process_skeleton_msg, skel)
 
     def test_process_skeleton_msg_raises_TypeError_if_joints_not_valid(self):
         bad_skel = nsku.generate_fake_skelmsg(['hhhead', 'ne33ck'])
         self.assertRaises(TypeError, self.skq._process_skeleton_msg, bad_skel)
-        
 
-    def _fill_skel_queue(self, nelements, joint_names, 
+    def _fill_skel_queue(self, nelements, joint_names,
                          skels_per_msg=2, label='label'):
         ''' Helper function that ads n nelements to the skeleton queue.
             parameters:
@@ -62,8 +62,9 @@ class TestSkeletonQueue(unittest.TestCase):
         '''
         msg_list = []
         for i in xrange(nelements):
-            q_item =  [nsku.generate_fake_NiteSkeletonList_msg(skels_per_msg, 
-                                                         joint_names), label]
+            fake_skels = nsku.generate_fake_NiteSkeletonList_msg(skels_per_msg,
+                                                                 joint_names)
+            q_item = [fake_skels, label]
             self.skq.skeleton_queue.append(q_item)
             msg_list.append(q_item)
         return msg_list
@@ -76,37 +77,36 @@ class TestSkeletonQueue(unittest.TestCase):
 
         list(self.skq._pop_from_queue(0))
         q_length = len(self.skq.skeleton_queue)
-        self.assertEqual(4, q_length,
-            "Queue should have remained untouched")
+        self.assertEqual(4, q_length, "Queue should have remained untouched")
 
         popped = list(self.skq._pop_from_queue(2))
         q_length = len(self.skq.skeleton_queue)
-        self.assertEqual(2, q_length,
-            "Queue should have now only 2 elements! "
-            "It has {} elements".format(q_length))
-       
+        self.assertEqual(2, q_length, "Queue should have now only 2 elements! "
+                                      "It has {} elements".format(q_length))
+
         # retrieving more elements that already are in queue
         popped = list(self.skq._pop_from_queue(4))
         q_length = len(self.skq.skeleton_queue)
-        self.assertEqual(q_length, 0,
-            "Queue should have been emptied")
-        self.assertEqual(len(popped), 2, 
-            "Should have retrieved the last {} elements from queue. "
-            "Retrieved: {}\nPopped elements: {}"
-            .format(2, len(popped), popped))
-       
+        self.assertEqual(q_length, 0, "Queue should have been emptied")
+        self.assertEqual(len(popped), 2,
+                         "Should have retrieved the last {} elems from queue. "
+                         "Retrieved: {}\nPopped elements: {}"
+                         .format(2, len(popped), popped))
+
     def test_pop_from_queue_does_not_return_messages_with_invalid_joints(self):
         # self.joint_names = ['head', 'neck']
         invalid_joint_names = ['aaaa', 'bbbb']
-        
+
         self._fill_skel_queue(4, invalid_joint_names, label='label')
         popped = list(self.skq._pop_from_queue(4))
         q_length = len(self.skq.skeleton_queue)
         self.assertEqual(q_length, 0, "Queue should have been emptied. "
-            "There still are {} elements in queue.".format(q_length))
+                                      "There still are {} elements in queue."
+                                      .format(q_length))
         self.assertEqual(len(popped), 0, "Shouldn't have returned any skeleton."
-            "Num of returned skeletons {}.".format(len(popped)))
-      
+                                         "Num of returned skeletons {}."
+                                         .format(len(popped)))
+
     def test_pop_from_queue_returns_a_skeleton_and_a_label(self):
         # self.joint_names = ['head', 'neck']
         label = 'label_1'
@@ -118,34 +118,35 @@ class TestSkeletonQueue(unittest.TestCase):
             item = list(item)
             message_skel = msg_list[i][0]
             message_label = msg_list[i][1]
-            unpacked_skel = nsku.unpack_skeleton_msg(message_skel.skeletons[0])[0]
+            unpacked_skel = \
+                nsku.unpack_skeleton_msg(message_skel.skeletons[0])[0]
             target_to_compare = list(chain(unpacked_skel, message_label))
-            self.assertEqual(item, target_to_compare, "Messages are not equal: "
-                "{} != {}".format(item, target_to_compare))
-        
+            self.assertEqual(item, target_to_compare,
+                             "Messages are not equal: {} != {}"
+                             .format(item, target_to_compare))
+
     def test_calc_chunksize(self):
         self.skq.skeleton_queue.clear()
         self.skq.skeleton_queue.extend(xrange(10))
         self.assertEqual(self.skq._calc_chunksize(8), 8)
         for i in [-1, 12]:
-            self.assertEqual(self.skq._calc_chunksize(i), 
+            self.assertEqual(self.skq._calc_chunksize(i),
                              len(self.skq.skeleton_queue))
-          
+
     # @unittest.skip("Skpping this Test")
     @patch.object(skq.SkeletonQueue, '_pop_from_queue')
     def test_prepare_chunk(self, mock_pop):
         # mock_pop.side_effect = lambda x: xrange(x)
-        
+
         self.skq.skeleton_queue.clear()
         self.assertEqual(list(self.skq._prepare_chunk()), [],
-            "Should return 'None' when queue is empty")
+                         "Should return 'None' when queue is empty")
 
-        # Testing 
+        # Testing
         self.skq.skeleton_queue.clear()
         self.skq.skeleton_queue.extend(xrange(10))
         list(self.skq._prepare_chunk(8))
-        mock_pop.assert_called_with(8)    
-
+        mock_pop.assert_called_with(8)
 
     def test_to_dataframe(self):
         '''ToDo'''
@@ -153,9 +154,5 @@ class TestSkeletonQueue(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    
-    # import rostest
-    # rostest.rosrun(PKG, 'test_PoseDatasetBuilder', TestPoseDatasetBuilder)
-    
     import rosunit
     rosunit.unitrun(PKG, 'test_skeleton_queue_IO', TestSkeletonQueue)
