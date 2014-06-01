171b3d6c4262ad0344244c02d6665a3ea1089a80
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-06-01 12:21:20 +0000

    new tests

---
+++ pose_instance_builder/src/test/test_calc_joint_velocities.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+'''
+@author: Victor Gonzalez Pacheco
+@date: 2014-05
+'''
+from __future__ import division
+PKG = 'pose_instance_builder'
+import roslib; roslib.load_manifest(PKG)
+# import rospy
+# import itertools as it
+import numpy as np
+import pandas as pd
+from joint_velocities_publisher import calc_velocities
+import unittest
+from numpy.testing import assert_array_almost_equal as assert_arrAlmostEQ
+
+class TestCalcJointVelocities(unittest.TestCase):
+    """Tests"""
+    def __init__(self, *args):
+        super(TestCalcJointVelocities, self).__init__(*args)
+
+    def setUp(self):
+        ''' :todo: load node
+            :todo: create dataframe that later will be used as instances
+        '''
+        self.df = pd.DataFrame(np.linspace(1,20,20).reshape(4,5),
+                               columns=list('ABCDE'))
+        # Pre-compute velocities
+        self.df_velocities = {1: np.array([1-1, 2-2, 3-3, 4-4, 5-5]),
+                              2: np.array([6-1, 7-2, 8-3, 9-4, 10-5]) / 2,
+                              3: np.array([11-1, 12-2, 13-3, 14-4, 15-5]) / 3,
+                              4: np.array([16-1, 17-2, 18-3, 19-4, 20-5]) / 4}
+        
+    def tearDown(self):
+        pass
+
+    def test_calc_velocities(self):
+        velocities = self.df_velocities[len(self.df)]
+        expected_velocities = calc_velocities(self.df)
+        assert_arrAlmostEQ(expected_velocities, velocities)
+        
+    def test_calc_velocities_returns_0_if_passed_a_single_row_dataframe(self):
+        df = pd.DataFrame()
+        df = df.append(pd.Series(range(5)), ignore_index=True)
+        expected_velocities = calc_velocities(df)
+        assert_arrAlmostEQ(expected_velocities, np.zeros(5))
+
+    def test_calc_velocities_raises_ValueError_if_dataframe_is_empty(self):
+        with self.assertRaises(ValueError):
+            calc_velocities(pd.DataFrame())
+
+
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun(PKG, 'test_calc_joint_velocities', TestCalcJointVelocities)
--- pose_instance_builder/src/test/test_joint_velocities_publisher.py
+++ pose_instance_builder/src/test/test_joint_velocities_publisher.py
@@ -6,57 +6,84 @@
 from __future__ import division
 PKG = 'pose_instance_builder'
 import roslib; roslib.load_manifest(PKG)
-
 import rospy
-import unittest
+from rospy import (logdebug, loginfo, logwarn, logerr, logfatal)
+
+# import itertools as it
 import numpy as np
 import pandas as pd
+from threading import Thread    # To run node in a separate thread
 
 from joint_velocities_publisher import JointVelocitiesPublisher
+from pose_instance_builder.msg import (PoseInstance, JointVelocities)
 
+import unittest
+# import numpy.testing 
+from numpy.testing import assert_array_almost_equal as assert_arrAlmostEQ
 
 class TestJointVelocitiesPublisher(unittest.TestCase):
     """Tests"""
     def __init__(self, *args):
         super(TestJointVelocitiesPublisher, self).__init__(*args)
-
+        name = 'test_joint_velocities_publisher'
+        rospy.init_node(name)
+                    
     def setUp(self):
-        ''' :todo: load node
-            :todo: create dataframe that later will be used as instances
-        '''
-        self.df = pd.DataFrame(np.linspace(1,20,20).reshape(4,5),
+        self.instances = pd.DataFrame(np.linspace(1,20,20).reshape(4,5),
                                columns=list('ABCDE'))
-        self.node = JointVelocitiesPublisher()
-        # node.run()
+        self.recvd_velocities = pd.DataFrame()
+        
+        rospy.Subscriber('/joint_velocities', JointVelocities, self._velo_cb)
+        self.publisher = rospy.Publisher('/pose_instance', PoseInstance)
 
     def tearDown(self):
+        # del(self.node)
         pass
-        # import rosnode
-        # rosnode.kill_nodes(['/instance_averager_node'])
 
-    def test_calc_velocities(self):
-        ''' give some datasets and check if velocities are correctly calculated'''
-        # Pre-compute results
-        velocities = np.array([16-1, 17-2, 18-3, 19-4, 20-5]) / 4
-        comparison = self.node.calc_velocities(self.df) == velocities
-        self.assertTrue(all(comparison))
-    
-    def test_calc_velocities_returns_0_if_passed_a_single_row_dataframe(self):
-        df = pd.DataFrame()
-        df = df.append(pd.Series(range(5)), ignore_index=True)
-        comparison = self.node.calc_velocities(df) == np.zeros(5)
-        self.assertTrue(all(comparison))
-        
-    def test_calc_velocities_raises_ValueError_if_dataframe_is_empty(self):
-        with self.assertRaises(ValueError):
-            self.node.calc_velocities(pd.DataFrame())
+    def test_joint_velocities_publihser_advertises_joint_velocities_topic(self):
+        rospy.sleep(0.3)  # Wait node is loaded
+        self.assertTrue(any(self._search_topic_name('joint_velocities')))
 
-
+    # @unittest.skip('Skipped')
     def test_publishes_velocities_when_receives_instances(self):
-        '''send df as pose_instances and check if returned velocities are ok
-           do it with a generator, so i can control the flow of messages calling
-           next()'''
-        pass
+        # Pre-computed velocities
+        expected_velocities = {1: np.array([0]*5),
+                                    2: np.array([2.5]*5),
+                                    3: np.array([3.33333333]*5),
+                                    4: np.array([3.75]*5)}
+        expected_velocities = pd.DataFrame(expected_velocities,
+                                                columns=list('ABCDE'))
+        
+        self._publish_instances(self.instances)
+        # rospy.sleep(1)
+        self.assertEqual(len(self.recvd_velocities), 
+                         len(expected_velocities),
+                         "lengths missmatch.\nReceived:\n{}\nExpected:\n{}"
+                         .format(self.recvd_velocities, expected_velocities))
+        for i, row in expected_velocities.iterrows():
+            self.assertSequenceEqual(self.recvd_velocities.iloc[i],
+                                     expected_velocities.iloc[i])
+
+
+    def _publish_instances(self, df):
+        for i, row in df.iterrows():
+            msg = PoseInstance(columns=row.index.tolist(),
+                               instance=row.values.tolist())
+            self.publisher.publish(msg)
+            
+
+    def _velo_cb(self, msg):
+        velocities = pd.Series(msg.velocities, index=msg.columns)
+        self.recvd_velocities = \
+            self.recvd_velocities.append(velocities, ignore_index=True)
+        
+
+    def _search_topic_name(self, name, namespace='/'):
+        ''' Returns a list of topics that contains name '''
+        all_topics = rospy.get_published_topics(namespace=namespace)
+        topic_names, topic_types = zip(*all_topics)
+        return filter(lambda s: name in s, topic_names)
+
 
 if __name__ == '__main__':
     import rostest
