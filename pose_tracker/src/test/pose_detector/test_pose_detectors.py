d819e866075b8cfa8e7bbbb559435221059a458c
Victor Gonzalez-Pacheco <victor.gonzalez.pacheco@gmail.com>
2014-06-05 18:14:03 +0000

    [BUG] corrected a bug in the test

--- pose_tracker/src/test/pose_detector/test_pose_detectors.py
+++ pose_tracker/src/test/pose_detector/test_pose_detectors.py
@@ -14,8 +14,8 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from pose_detector import (is_dataset_full, is_still, is_moving,
-                           DatasetNotFullError)
+from pose_detector.pose_detector_node import (is_dataset_full, is_still, 
+                                              is_moving, DatasetNotFullError)
 
 class TestIsDatasetFull(unittest.TestCase):
     """Tests"""
@@ -23,13 +23,13 @@ class TestIsDatasetFull(unittest.TestCase):
         super(TestIsDatasetFull, self).__init__(*args)
         # name = 'test_pose_detector'
         # rospy.init_node(name)
-                    
+
     def setUp(self):
         # datasets with 1,2,3,4 rows and 5 columns
         self.datasets = (pd.DataFrame(np.linspace(1,5,5).reshape(1,5)),
                          pd.DataFrame(np.linspace(1,10,10).reshape(2,5)),
-                         pd.DataFrame(np.linespace(1,15,15).reshape(3,5)),
-                         pd.DataFrame(np.linespace(1,20,20).reshape(4,5)))
+                         pd.DataFrame(np.linspace(1,15,15).reshape(3,5)),
+                         pd.DataFrame(np.linspace(1,20,20).reshape(4,5)))
 
     def tearDown(self):
         pass
@@ -37,20 +37,20 @@ class TestIsDatasetFull(unittest.TestCase):
     def test_is_dataset_full_raises_if_not_full(self):
         for i, expected_length in enumerate([2, 3, 4, 5]):
             with self.assertRaises(DatasetNotFullError):
-                dataset_length = len(self.datasets[i])
-                is_dataset_full(expected_length, dataset_length)
+                # dataset_length = len(self.datasets[i])
+                is_dataset_full(expected_length, self.datasets[i])
 
     def test_is_dataset_full_raises_if_dataset_length_greater_than_length(self):
         for i, expected_length in enumerate([0, 1, 1, 1]):
-            dataset_length = len(self.datasets[i])
+            # dataset_length = len(self.datasets[i])
             with self.assertRaises(DatasetNotFullError):
-                is_dataset_full(expected_length, dataset_length)
+                is_dataset_full(expected_length, self.datasets[i])
 
     def test_is_dataset_full_does_not_raise(self):
         for i, expected_length in enumerate([1, 2, 3, 4]):
-            dataset_length = len(self.datasets[i])
+            # dataset_length = len(self.datasets[i])
             try:
-                is_dataset_full(expected_length, dataset_length)
+                is_dataset_full(expected_length, self.datasets[i])
             except DatasetNotFullError as dnf:
                 self.fail(dnf) 
 
@@ -114,20 +114,17 @@ class TestIsMoving(unittest.TestCase):
         self.tens.iloc[1,1] = -13
         self.zeros.iloc[1,1] = 25
         for dataset in [self.zeros, self.fives, self.tens]:
-            self.assertFalse(is_still(6, dataset))
+            self.assertFalse(is_moving(6, dataset))
         
-
     def returns_false_if_some_data_is_equal_to_threshold(self):
         for dataset in self.datasets:
-            self.assertFalse(is_still(len(dataset), dataset))
+            self.assertFalse(is_moving(len(dataset), dataset))
 
     def returns_True_if_all_data_is_above_threshold(self):
         for dataset in self.datasets[1:]:
-            self.assertTrue(is_still(0, dataset))
+            self.assertTrue(is_moving(0, dataset))
 
         
-   
-
 if __name__ == '__main__':
     import rosunit
     rosunit.unitrun(PKG, 'test_is_dataset_full', TestIsDatasetFull)
