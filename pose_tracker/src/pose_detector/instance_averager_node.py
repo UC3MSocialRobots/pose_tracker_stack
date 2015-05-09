0ee532d6ff4edc94c0174bb800cc280bfbab04b9
Victor González-Pacheco <victor.gonzalez.pacheco@gmail.com>
2015-05-09 17:28:33 +0200

    [FIX] calling load_params with too many args

--- pose_tracker/src/pose_detector/instance_averager_node.py
+++ pose_tracker/src/pose_detector/instance_averager_node.py
@@ -58,8 +58,8 @@ class InstanceAveragerNode():
 
         with eh(logger=logfatal, log_msg="Couldn't load parameters",
                 action=self.shutdown, reraise=True):
-                self.method, self.dflen = load_params('averager_method',
-                                                      'dataframe_length')
+                self.method, self.dflen = load_params(['averager_method',
+                                                      'dataframe_length'])
                 self.averager = METHODS.get(self.method, METHODS['mean'])
 
         # Publishers and Subscribers
