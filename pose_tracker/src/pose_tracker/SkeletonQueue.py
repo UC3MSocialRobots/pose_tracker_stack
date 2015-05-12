61134222b5c0195371ab2d5e4fb87e482e3206de
Victor Gonzalez Pacheco <victor.gonzalez.pacheco@gmail.com>
2015-05-12 21:44:00 +0200

    SkeletonQueue.py: Docstring improvement & PEP257
    Improved docsctring documentation and also added PEP257 compliance.

--- pose_tracker/src/pose_tracker/SkeletonQueue.py
+++ pose_tracker/src/pose_tracker/SkeletonQueue.py
@@ -9,8 +9,12 @@ import kinect.nite_skeleton_msg_utils as nsku
 
 
 class SkeletonQueue(object):
-    """Class that contains a queue of skeletons
-       along with operations return its eleements as a pandas.DataFrame"""
+
+    """
+    Class that contains a queue of skeletons
+    along with operations return its eleements as a pandas.DataFrame
+    """
+
     def __init__(self, joint_names):
         super(SkeletonQueue, self).__init__()
         self.skeleton_queue = col.deque([])  # stores (skeletons, label)
@@ -20,15 +24,15 @@ class SkeletonQueue(object):
         return len(self.skeleton_queue)
 
     def append(self, skeletons, label):
-        """appends a skeleton and a label to the queue """
+        """Append a skeleton and a label to the queue."""
         self.skeleton_queue.append((skeletons, label))
 
     def clear(self):
-        """removes all elements of the queue"""
+        """Remove all elements of the queue."""
         self.skeleton_queue.clear()
 
     def _check_joints(self, joint_names1, joint_names2):
-        """Raises TypeError if entered parameters differ"""
+        """Raise TypeError if entered parameters differ."""
         if set(joint_names1) != set(joint_names2):
             rospy.logwarn("Message joint name != expected one")
             raise TypeError(
@@ -37,8 +41,12 @@ class SkeletonQueue(object):
                 .format(str(joint_names1), str(joint_names2)))
 
     def _process_skeleton_msg(self, skeleton):
-        """Unpacks a skeleton message frome the queue,
-            checks if joints are valid and returns its data """
+        """
+        Return unpacked data from a skeleton if its joints are valid..
+
+        Unpack a skeleton message frome the queue,
+        check if joints are valid and returns its data.
+        """
         try:
             data, joint_names = nsku.unpack_skeleton_msg(skeleton)
             self._check_joints(joint_names, self.joint_names)
@@ -47,15 +55,17 @@ class SkeletonQueue(object):
             raise e
 
     def _calc_chunksize(self, chunksize):
-        """Calculates how many elements to retrieve from squeleton queue"""
+        """Calculate how many elements to retrieve from squeleton queue."""
         if chunksize < 0 or chunksize > len(self.skeleton_queue):
             return len(self.skeleton_queue)
         return chunksize
 
     def _pop_from_queue(self, elements):
-        """Generator that pops n elements from skeleton_queue.
-            @note: this is a generator, meaning that it executes LAZYLY!
-            @note: only grabs the first skeleton from the NiteSkeletonList msg
+        """
+        Pop n elements from skeleton_queue.
+
+        @note: this is a generator, meaning that it executes LAZYLY!
+        @note: only grabs the first skeleton from the NiteSkeletonList msg
         """
         for _ in xrange(self._calc_chunksize(elements)):
             try:
@@ -70,10 +80,12 @@ class SkeletonQueue(object):
                 break
 
     def _prepare_chunk(self, chunksize=50):
-        """Yields chunksize elements from self.skeleton_queue (using popleft())
-            @param chunksize: Num of elements to retrieve from the queue.
-            @note: If chunksize < 0  or > len self.skeleton_queue,
-                then yelds all elements of the queue
+        """
+        Yield chunksize elements from self.skeleton_queue (using popleft()).
+
+        @param chunksize: Num of elements to retrieve from the queue.
+        @note: If chunksize < 0  or > len self.skeleton_queue,
+            then yelds all elements of the queue
         """
         if not self.skeleton_queue:
             rospy.logdebug("Skeleton_queue is empty. Nothing to do.")
@@ -83,21 +95,26 @@ class SkeletonQueue(object):
             yield skel_data
 
     def _chunk_to_data_frame(self, chunk, columns):
-        """Returns a pandas.DataFrame of chunksize elements.
-            @note: This method expects its input from L{_prepare_chunk}
-            @param chunk: a chunk of elements obtained from L{_prepare_chunk}
-            @param columns: list containing the names of the
-                pandas.DataFrame columns
-            @return: the elements of queue in form of a pandas.DataFrame
-            @rtype: pandas.Dataframe"""
+        """
+        Return a pandas.DataFrame of chunksize elements.
+
+        @note: This method expects its input from L{_prepare_chunk}
+        @param chunk: a chunk of elements obtained from L{_prepare_chunk}
+        @param columns: list containing the names of the
+            pandas.DataFrame columns
+        @return: the elements of queue in form of a pandas.DataFrame
+        @rtype: pandas.Dataframe
+        """
         return pd.DataFrame(list(chunk), columns=columns)
 
     def pop_n_to_DataFrame(self, n, columns):
-        """Pops n elements from the queue
-            and returns them as a L{pandas.DataFrame}
-            @param n: number of elements to retrieve from queue
-            @param columns: list with the names of the pandas.DataFrame columns
-            @return: n elements from the queue in form of a pandas.DataFrame
-            @rtype: pandas.DataFrame"""
+        """
+        Pop n elements from queue and returns them as a L{pandas.DataFrame}.
+
+        @param n: number of elements to retrieve from queue
+        @param columns: list with the names of the pandas.DataFrame columns
+        @return: n elements from the queue in form of a pandas.DataFrame
+        @rtype: pandas.DataFrame
+        """
         chunk = self._prepare_chunk(n)
         return self._chunk_to_data_frame(chunk, columns)
