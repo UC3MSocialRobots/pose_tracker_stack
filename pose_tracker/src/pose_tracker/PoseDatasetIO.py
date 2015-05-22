093028ef118fba42c4cfb52747db686ddf38d7db
Victor Gonzalez Pacheco <victor.gonzalez.pacheco@gmail.com>
2015-05-22 23:59:31 +0200

    Using triple double quotes

--- pose_tracker/src/pose_tracker/PoseDatasetIO.py
+++ pose_tracker/src/pose_tracker/PoseDatasetIO.py
@@ -8,7 +8,7 @@ import datetime
 
 
 def parse_date(date):
-    ''' An utility function to parse the date used in the dataset metadata.
+    """ An utility function to parse the date used in the dataset metadata.
 
         @param date: The date in a form of a string. Must follow ISO8601:
             1. I{YYYY-MM-DD}
@@ -17,7 +17,7 @@ def parse_date(date):
         @see: ISO 8601 for more information.
         @return: the entered date in form of "YYYY-MM-DD hh:mm:ss"
         @raise ValueError: if entered date is malformed
-        '''
+        """
     supported_formats = ('%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d')
     for fmt in supported_formats:
         try:
@@ -35,18 +35,18 @@ def parse_date(date):
 
 
 def filename_with_extension(name, extension):
-    ''' Ensures filename ends with extension.
+    """ Ensures filename ends with extension.
 
         @return: name if name ends with extension
         @return: name + etension if name does not end with extension
-    '''
+    """
     return name if name.endswith(extension) else name + extension
 
 
 class PoseDatasetIO(object):
     """Class that that reads/writes data to a dataset containing poses"""
     def __init__(self, *args, **kwargs):
-        ''' Inits the class.
+        """ Inits the class.
 
             @param dataset: name of the file where the dataset will be written
             @param columns: name of the columns for the dataset_table
@@ -60,7 +60,7 @@ class PoseDatasetIO(object):
 
             @raise KeyError: if some argument is missing
             @raise TypeError: if dataset is not string or columns is
-            not an iterable.'''
+            not an iterable."""
 
         #self.dataset = kwargs['dataset'] + '.h5'
         self.dataset = filename_with_extension(kwargs['dataset'], '.h5')
@@ -73,7 +73,7 @@ class PoseDatasetIO(object):
             raise TypeError("dataset columns must be iterable!")
 
     def __enter__(self):
-        ''' @todo allow open dataset passing it arguments'''
+        """ @todo allow open dataset passing it arguments"""
         self.open_dataset(mode=self._mode)
         return self
         # self.create_dataset(self.dataset)
@@ -83,11 +83,11 @@ class PoseDatasetIO(object):
         return
 
     def create_dataset(self):
-        '''Creates de dataset file in HDF5 Format'''
+        """Creates de dataset file in HDF5 Format"""
         self.open_dataset()
 
     def open_dataset(self, **kwargs):
-        ''' Opens the dataset file using the pandas.HDFStore API.
+        """ Opens the dataset file using the pandas.HDFStore API.
             @see: pandas.HDFStore
             @note: All keywords are passed to the pandas.HDFStore API
             @type path: string
@@ -99,21 +99,21 @@ class PoseDatasetIO(object):
                 - ``'a'`` Append; an existing file is opened for reading
                           and writing. If the file does not exist it is created.
                 - ``'r+'`` Similar to ``'a'``, but the file must already exist.
-        '''
+        """
         self.store = pd.HDFStore(self.dataset, **kwargs)
 
     def close(self):
-        '''Closes the dataset file'''
+        """Closes the dataset file"""
         self.store.close()
 
     # def prepare_dataset(self, **kwargs):
     def fill_metadata(self, **kwargs):
-        ''' Fills the metadata of the dataset.
+        """ Fills the metadata of the dataset.
 
             @keyword creator: The creator of the dataset
             @keyword date: Date in "YYYY-mm-ddTHH:MM" (ISO 8601 date format)'
             @keyword descr: string based description of the dataset content
-       '''
+       """
         creator = kwargs.get('creator', 'Anonymous')
         date = kwargs.get('date', '1900-01-01 00:00')
         user_descr = kwargs.get('descr',
@@ -127,15 +127,15 @@ class PoseDatasetIO(object):
         self.store.put('description', description)
 
     def get_metadata(self):
-        ''' Returns the metadata table from the file
+        """ Returns the metadata table from the file
 
             @see: L{fill_metadata}
             @return: the metadata contained in the dataset file
-            @rtype: pandas:Series'''
+            @rtype: pandas:Series"""
         return self.read_table('description')
 
     def write(self, table_name, chunk, **kwargs):
-        ''' Writes a chunk of data to the dataset file
+        """ Writes a chunk of data to the dataset file
 
             @param table_name: the name of the table on the file
             @ptype chunk: pandas.DataFrame
@@ -146,7 +146,7 @@ class PoseDatasetIO(object):
             @raise TypeError: if chunk is not a pandas.DataFrame
             @raise ValueError: if chunk is empty
 
-            '''
+            """
         if not isinstance(chunk, pd.DataFrame):
             raise TypeError("chunk is not a pandas.DataFrame. Could not write")
 
@@ -157,22 +157,22 @@ class PoseDatasetIO(object):
         self.store.put(table_name, chunk, **kwargs)
 
     def read_table(self, table_name):
-        ''' Reads the file and returns a dataframe stored in table table_name
+        """ Reads the file and returns a dataframe stored in table table_name
 
         @type table_name: string
         @param table_name: The table where to read
         @return: a pandas dataframe obtained from table table_name
-        @rtype: pandas.DataFrame'''
+        @rtype: pandas.DataFrame"""
         return self.store.get(table_name)
 
     def read_group(self, group_name):
-        ''' Reads the file and returns a dictionary with all tables belonging to
+        """ Reads the file and returns a dictionary with all tables belonging to
             a group
 
             @type table_name: string
             @param table_name: The table where to read
             @return: a dict whose keys refer to dataframes obtained from table table_name
-            @rtype: dict wich values are pandas.DataFrames'''
+            @rtype: dict wich values are pandas.DataFrames"""
         #return pd.concat([self.store.select(node._v_pathname)
         #                  for node in self.store.get_node(group_name)])
         return {node._v_name: self.store.select(node._v_pathname)
